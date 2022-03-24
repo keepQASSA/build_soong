791b59a0f9a0fddc237a8bd9f75836dc03951933
ghou8s <ghou8s@gmail.com>
2022-03-24 19:50:07 +0000

    build/soong: adapt to aosqp
    Signed-off-by: ghou8s <ghou8s@gmail.com>
    Change-Id: Ifa02ec9736f3e0790791eecf058fb026a1ac0c83

--- android/variable.go
+++ android/variable.go
@@ -20,7 +20,7 @@ import (
 	"runtime"
 	"strings"
 
-	"aosp/soong/android"
+	"aosqp/soong/android"
 
 	"github.com/google/blueprint/proptools"
 )
@@ -127,8 +127,8 @@ type variableProperties struct {
 			Srcs         []string
 		}
 
-		// include Aosp variables
-		Aosp android.Product_variables
+		// include Aosqp variables
+		Aosqp android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -293,8 +293,8 @@ type productVariables struct {
 
 	TargetFSConfigGen []string `json:",omitempty"`
 
-	// include Aosp variables
-	Aosp android.ProductVariables
+	// include Aosqp variables
+	Aosqp android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
