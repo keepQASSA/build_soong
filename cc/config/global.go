5265d4e381653cce22f39d7c56544be494ee54b0
ghou8s <ghou8s@gmail.com>
2022-09-28 13:07:34 +0530

    Revert "soong: global: Bump renderscript build clang to 9.0.6"
    This reverts commit afb1e5176f65418857be5ff3a70c8f5729b7bfcd.
    
    * reason revert: 1998 Cam crash, thanks to @agilyudaa for report. log https://bin.kv2.dev/~63313713e599576ea02ae020

--- cc/config/global.go
+++ cc/config/global.go
@@ -42,8 +42,8 @@ var (
 		"-fno-exceptions",
 		"-Wno-multichar",
 
-		"-O3",
-		"-g0",
+		"-O2",
+		"-g",
 
 		"-fno-strict-aliasing",
 	}
@@ -215,8 +215,8 @@ func init() {
 	// These are tied to the version of LLVM directly in external/llvm, so they might trail the host prebuilts
 	// being used for the rest of the build process.
 	pctx.SourcePathVariable("RSClangBase", "prebuilts/clang/host")
-	pctx.SourcePathVariable("RSClangVersion", "clang-r353983c1")
-	pctx.SourcePathVariable("RSReleaseVersion", "9.0.3")
+	pctx.SourcePathVariable("RSClangVersion", "clang-3289846")
+	pctx.SourcePathVariable("RSReleaseVersion", "3.8")
 	pctx.StaticVariable("RSLLVMPrebuiltsPath", "${RSClangBase}/${HostPrebuiltTag}/${RSClangVersion}/bin")
 	pctx.StaticVariable("RSIncludePath", "${RSLLVMPrebuiltsPath}/../lib64/clang/${RSReleaseVersion}/include")
 
